56f0fee7b1b80ca8b4a0840bcd502890a3738beb
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-04-06 11:14:18 -0700

    Add import Wasm

--- public/main.js
+++ public/main.js
@@ -1,12 +1,19 @@
-function init() {
+async function init() {
+  let rustApp = null;
+  
+  try {
+    rustApp = await import("../pkg");
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+  console.log(rustApp);
+
   const input = document.getElementById("upload");
   const fileReader = new FileReader();
 
   fileReader.onloadend = () => {
-    let base64 = fileReader.result.replace(
-      /^data:(.*;base64,)?/,
-      ""
-    );
+    let base64 = fileReader.result.replace(/^data:(.*;base64,)?/, "");
     console.log(input.files[0]);
     console.log(base64);
   };
