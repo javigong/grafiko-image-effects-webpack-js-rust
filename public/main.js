ec8b1ba5ab9d561470e541fb347f72d252a23c31
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-04-26 18:11:41 -0700

    Add base64 decoding

--- public/main.js
+++ public/main.js
@@ -3,28 +3,28 @@ async function init() {
 
   try {
     // import wasm from the pkg folder
-    rustApp = await import("../pkg/index.js");
-  
-    console.log(rustApp.length());
-
-    const input = document.getElementById("upload");
-    const fileReader = new FileReader();
-
-    fileReader.onloadend = () => {
-      let base64 = fileReader.result.replace(
-        /^data:image\/(png|jpeg|jpg);base64,/,
-        ""
-      );
-      let image = rustApp.grayscale(base64);
-    };
-
-    input.addEventListener("change", () => {
-      fileReader.readAsDataURL(input.files[0]);
-    });
+    rustApp = await import("../pkg");
   } catch (e) {
     console.error(e);
     return;
   }
+
+  console.log(rustApp);
+
+  const input = document.getElementById("upload");
+  const fileReader = new FileReader();
+
+  fileReader.onloadend = () => {
+    let base64 = fileReader.result.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/,
+      ""
+    );
+    rustApp.grayscale(base64);
+  };
+
+  input.addEventListener("change", () => {
+    fileReader.readAsDataURL(input.files[0]);
+  });
 }
 
 init();
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
+use base64::decode;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) {
-    log(&encoded_file.into());
+    log(&"Grayscale called".into());
+
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"Image decoded".into());
 }
