2ff414d7ebcd4e0bc2a0d187de43992a5376e7a7
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-04-26 18:31:51 -0700

    add buffer to write the image

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
 use base64::decode;
 use image::load_from_memory;
+use image::ImageOutputFormat::Png;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) {
@@ -15,4 +16,8 @@ pub fn grayscale(encoded_file: &str) {
 
     img = img.grayscale();
     log(&"Grayscale effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"New image written".into());
 }
