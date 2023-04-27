1349a837dce4697f1029ecfa87d05011bd2125b6
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-04-26 18:16:48 -0700

    Add loading image from memory

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
 use base64::decode;
+use image::load_from_memory;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) {
@@ -8,4 +9,7 @@ pub fn grayscale(encoded_file: &str) {
 
     let base64_to_vector = decode(encoded_file).unwrap();
     log(&"Image decoded".into());
+
+    let img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image loaded".into());
 }
