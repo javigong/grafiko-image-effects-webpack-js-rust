10853fcc1d1adccf0bb1fd9e1cb75fb67eb6918d
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-04-26 18:49:04 -0700

    Add encoding image

--- public/main.js
+++ public/main.js
@@ -19,7 +19,8 @@ async function init() {
       /^data:image\/(png|jpeg|jpg);base64,/,
       ""
     );
-    rustApp.grayscale(base64);
+    let img_data_url = rustApp.grayscale(base64);
+    document.getElementById("new-img").setAttribute("src", img_data_url);
   };
 
   input.addEventListener("change", () => {
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
-use base64::decode;
+use base64::{ encode, decode};
 use image::load_from_memory;
 use image::ImageOutputFormat::Png;
 
 #[wasm_bindgen]
-pub fn grayscale(encoded_file: &str) {
+pub fn grayscale(encoded_file: &str) -> String {
     log(&"Grayscale called".into());
 
     let base64_to_vector = decode(encoded_file).unwrap();
@@ -20,4 +20,12 @@ pub fn grayscale(encoded_file: &str) {
     let mut buffer = vec![];
     img.write_to(&mut buffer, Png).unwrap();
     log(&"New image written".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!(
+        "data:image/png;base64,{}",
+        encoded_img
+    );
+
+    data_url
 }
